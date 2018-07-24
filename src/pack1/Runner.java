63eac1edbb295f1ab2948427c9bf72dc0eb5c6c7
CaptainSparky <josh@handsonit.co.uk>
2018-07-24 09:48:23 +0100

    added a startup test and inital setup parts

--- src/pack1/Runner.java
+++ src/pack1/Runner.java
@@ -2,4 +2,14 @@ package pack1;
 
 public class Runner {
 
+	
+	public static void main(String[] args) {
+		computation();
+
+	}
+
+	private static void computation() {
+		// TODO Auto-generated method stub
+		
+	}
 }
--- src/pack1/Sort.java
+++ src/pack1/Sort.java
@@ -1,11 +1,30 @@
 package pack1;
 
 public class Sort {
-
+ int[] digits;
+ 
 	public Sort(int i) {
-		// TODO Auto-generated constructor stub
+		int length = String.valueOf(i).length();
+		digits = new int[length-1];
+		fill(i, length);
 	}
 
-	public int convert;
+
+	private void fill(int i, int length) {
+		
+		String temp = Integer.toString(i);
+		for (int inc = 0; inc < length-1;){
+			digits[inc] = temp.charAt(inc);
+		}
+		
+	}
+
+
+	public int convert() {
+		
+		
+		
+		return 0;
+	}
 
 }
